c76aab8c73d6a8720d8b6ef9e482a5b66190a515
paolocorti <paolocorti87@gmail.com>
2023-01-12 15:37:49 +0100

    testing memory leak when rerendering componet

--- examples/image-tracking/src/App.js
+++ examples/image-tracking/src/App.js
@@ -1,4 +1,5 @@
 import { ARAnchor, ARView } from "react-three-mind";
+import React, { useState } from "react";
 
 function Plane(props) {
   return (
@@ -9,7 +10,10 @@ function Plane(props) {
   );
 }
 
-function App() {
+function Slide({ activeIndex }) {
+  console.log(activeIndex);
+  const [scan, setScan] = useState(false);
+
   return (
     <ARView
       imageTargets="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/card.mind"
@@ -20,10 +24,36 @@ function App() {
     >
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <ARAnchor target={0}>
+      <ARAnchor
+        target={0}
+        onAnchorFound={() => {
+          console.log("onAnchorFound");
+          setScan(false);
+        }}
+        onAnchorLost={() => {
+          console.log("onAnchorLost");
+          setScan(true);
+        }}
+      >
         <Plane />
       </ARAnchor>
     </ARView>
+  );
+}
+
+function App() {
+  const [active, setActive] = useState(0);
+
+  return (
+    <div>
+      <button
+        style={{ position: "absolute", zIndex: 99999 }}
+        onClick={() => setActive(active + 1)}
+      >
+        change index
+      </button>
+      <Slide activeIndex={active} />
+    </div>
   );
 }
 
